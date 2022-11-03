010bf166a1e61ae4ef57c0ded9fb3b185c5163c4
Muhammad Haris <muhammadharisjan5@gmail.com>
2022-11-04 01:30:00 +0500

    cors update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const auth = require('./route/auth.route')
 const transaction = require('./route/transaction.auth')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json());
-
+app.use(cors())
 app.use("/api/v1/auth", auth)
 app.use('/api/v1/transaction', transaction)
 module.exports = app
